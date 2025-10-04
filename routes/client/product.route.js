c8d3adf143feb80e111bcfd8bb5fd7796a959231
Vo Van Quyen <vvq0522@gmail.com>
2025-10-04 23:20:03 +0700

    controller congfiguration

---
+++ routes/client/product.route.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../../controllers/client/product.controller')
+
+router.get('/products', controller.index)
+
+module.exports = router
