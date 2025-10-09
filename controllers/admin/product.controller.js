3e259523d60ac57dfff436794c7548a4cca8e381
Vo Van Quyen <vvq0522@gmail.com>
2025-10-09 20:42:12 +0700

    Fectch data and rende it on the product page

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,15 @@
-module.exports.index = (req, res) => {
+const Product = require('../../models/product.model')
+
+// [GET] /admin/products
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    })
+
+    console.log(products)
+
     res.render('admin/pages/product/index', {
-        pageTitle: 'Trang sản phẩm'
+        pageTitle: 'Trang sản phẩm',
+        products: products
     })
 }
