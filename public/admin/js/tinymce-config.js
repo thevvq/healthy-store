3071597f85be69ea6ca42d2c927641a955c8eba5
Văn <vvq0522@gmail.com>
2025-12-05 19:52:08 +0700

    Fix tinyMCE

--- public/admin/js/tinymce-config.js
+++ public/admin/js/tinymce-config.js
@@ -1,7 +1,11 @@
 tinymce.init({
-  selector: 'textarea',
-  plugins: 'lists link image table',
-  toolbar: 'undo redo | bold italic | bullist numlist | link image table',
-  menubar: true
+    selector: 'textarea',
+    plugins: 'lists link image table',
+    toolbar: 'undo redo | bold italic | bullist numlist | link image table',
+    menubar: true,
+
+    setup: function (editor) {
+        editor.on('change', () => editor.save());
+    }
 });
 
